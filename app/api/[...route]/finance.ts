43bf9882950755df36828d500d91295149eee186
Jamin <jaminwebdev@gmail.com>
2024-10-01 12:08:11 -0500

    add finance route basics

---
+++ app/api/[...route]/finance.ts
@@ -0,0 +1,154 @@
+import { Hono } from 'hono';
+import { zValidator } from '@hono/zod-validator';
+import { generateMock } from '@anatine/zod-mock';
+import { z } from 'zod';
+import { faker } from '@faker-js/faker';
+import { flattenMapToArrayAndSortByDate } from '../../utils/helpers';
+
+const transactionSchema = z.object({
+  uid: z.string().nonempty(),
+  date: z.date(),
+  accountNumber: z.string(),
+  transactionDescription: z.string(),
+  transactionType: z.string(),
+  amount: z.string(),
+});
+
+type transactionType = z.infer<typeof transactionSchema>;
+
+const transactionsCache = new Map<string, transactionType>();
+
+const accountSchema = z.object({
+  uid: z.string().nonempty(),
+  accountName: z.string(),
+  balance: z.number().min(0),
+  date: z.date()
+});
+
+type accountType = z.infer<typeof accountSchema>;
+
+const accountsCache = new Map<string, accountType>();
+
+const generateAccount = () => {
+  const mockAccount = generateMock(accountSchema)
+  mockAccount.date = new Date()
+  return mockAccount
+}
+
+const generateTransaction = () => {
+  const mockTransaction  = generateMock(transactionSchema);
+  mockTransaction.date = new Date()
+  if (accountsCache.size <= 0) {
+    const mockAccount = generateAccount()
+    accountsCache.set(mockAccount.uid, mockAccount);
+    mockTransaction.accountNumber = mockAccount.uid
+    return mockTransaction
+  }
+  const randomAccountUid = [...accountsCache.keys()][Math.floor(Math.random() * accountsCache.size)];
+  mockTransaction.accountNumber = randomAccountUid;
+  return mockTransaction
+}
+
+const app = new Hono()
+	.get('/transactions', 
+    async (c) => {
+      const force = c.req.query('force_clear');
+      
+      if (transactionsCache.size >= 1500) {
+        const transactionsArray = flattenMapToArrayAndSortByDate(transactionsCache)
+        return c.json({ transactions: transactionsArray });
+      }
+
+      if (force) transactionsCache.clear();
+
+      const mockTransaction = generateTransaction()
+
+      transactionsCache.set(mockTransaction.uid, mockTransaction);
+
+      const transactionsArray = flattenMapToArrayAndSortByDate(transactionsCache)
+      return c.json({ transactions: transactionsArray });
+	})
+  .get('/transactions/:uid', async (c) => {
+    const { uid } = c.req.param();
+
+    const transaction = transactionsCache.get(uid);
+
+    if (!transaction) {
+      return c.json({ error: 'Transaction not found' }, 404);
+    }
+
+    return c.json({ transaction });
+  })
+  .get('transactions/generate/:count', async (c) => {
+    const { count } = c.req.param();
+
+    if (transactionsCache.size >= 1500) {
+      const transactionsArray = flattenMapToArrayAndSortByDate(transactionsCache)
+      return c.json({ transactions: transactionsArray });
+    }
+
+    for (let i = 0; i <= parseInt(count); i++) {
+      const mockTransaction = generateTransaction();
+      transactionsCache.set(mockTransaction.uid, mockTransaction);
+    }
+
+    const transactionsArray = flattenMapToArrayAndSortByDate(transactionsCache)
+    return c.json({ transactions: transactionsArray });
+  })
+  .patch('/transactions/:uid', 
+		zValidator(
+			'param',
+			z.object({
+				uid: z.string().optional()
+			})
+		),
+		zValidator('json', transactionSchema.partial()),
+    async (c) => {
+    const { uid } = c.req.valid('param');
+		const values = c.req.valid('json');
+
+    if (!uid) {
+      return c.json({ error: 'Must provide a unique id' }, 400);
+    }
+    
+    const transaction = transactionsCache.get(uid);
+
+    if (!transaction) {
+      return c.json({ error: 'User not found' }, 404);
+    }
+
+    transactionsCache.set(uid, { ...transaction, ...values });
+    return c.json({ transaction: transactionsCache.get(uid) });
+  })
+  .delete('/transactions/:uid', async (c) => {
+    const { uid } = c.req.param();
+    const transaction = transactionsCache.get(uid);
+
+    if (!transaction) {
+      return c.json({ error: 'User not found' }, 404);
+    }
+
+    transactionsCache.delete(uid);
+
+    return c.json({ message: 'Transaction deleted' });
+  })
+  .get('/accounts', async (c) => {
+    const force = c.req.query('force_clear');
+      
+    if (accountsCache.size >= 50) {
+      const accountsArray = flattenMapToArrayAndSortByDate(accountsCache)
+      return c.json({ accounts: accountsArray });
+    }
+
+    if (force) accountsCache.clear();
+
+    const mockAccount = generateAccount()
+
+    accountsCache.set(mockAccount.uid, mockAccount);
+
+    const accountsArray = flattenMapToArrayAndSortByDate(accountsCache)
+    return c.json({ accounts: accountsArray });
+  })
+	
+
+export default app;
--- app/api/[...route]/route.ts
+++ app/api/[...route]/route.ts
@@ -2,6 +2,7 @@ import { Hono } from 'hono';
 import { requestId } from 'hono/request-id';
 import { handle } from 'hono/vercel';
 import users from './users';
+import finance from './finance'
 
 export const runtime = 'edge';
 
@@ -11,6 +12,7 @@ app.use('*', requestId());
 
 const routes = app
 	.route('/users', users)
+  .route('/finance', finance)
 
 export const GET = handle(app);
 export const POST = handle(app);
