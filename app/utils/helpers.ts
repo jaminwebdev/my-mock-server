481b910c4ddbb6cf7a974d30a16b9badd6bb189a
Jamin <jaminwebdev@gmail.com>
2024-10-02 11:08:01 -0500

    fix date bug

--- app/utils/helpers.ts
+++ app/utils/helpers.ts
@@ -3,7 +3,7 @@ export const flattenMapToArray = (map: Map<string, any>) => {
 };
 
 export const flattenMapToArrayAndSortByDate = (map: Map<string, any>) => {
-  return flattenMapToArray(map).sort((a, b) => a.date.getTime() - b.date.getTime());
+  return flattenMapToArray(map).sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
 };
 
 export const flattenMapToArrayAndSortByPrice = (map: Map<string, any>) => {
